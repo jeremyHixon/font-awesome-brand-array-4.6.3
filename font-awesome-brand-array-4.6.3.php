211aaf32b63f640eefbd5b50783030dceac6a6c4
Jeremy Hixon <jhixon+gitlab@gmail.com>
2016-09-22 19:55:30 -0400

    Initial commit

---
+++ font-awesome-brand-array-4.6.3.php
@@ -0,0 +1,478 @@
+<?php
+$fa_brand_icons = array(
+	'500px.com'	=> array(
+		'color'		=> '#0099e5',
+		'name'		=> '500px',
+		'unicode'	=> '\f26e ',
+	),
+	'amazon.com'	=> array(
+		'color'		=> '#ff9900',
+		'name'		=> 'amazon',
+		'unicode'	=> '\f270 ',
+	),
+	'angel.co'	=> array(
+		'color'		=> '#343434',
+		'name'		=> 'angellist',
+		'unicode'	=> '\f209',
+	),
+	'behance.net'	=> array(
+		'color'		=> '#1769ff',
+		'name'		=> 'behance',
+		'unicode'	=> '\f1b4',
+	),
+	'bitbucket.org'	=> array(
+		'color'		=> '#205081',
+		'name'		=> 'bitbucket',
+		'unicode'	=> '\f171',
+	),
+	'bitcoin.org'	=> array(
+		'color'		=> '#f7931a',
+		'name'		=> 'bitcoin',
+		'unicode'	=> '\f15a',
+	),
+	'blacktie.io'	=> array(
+		'color'		=> '#000000',
+		'name'		=> 'black-tie',
+		'unicode'	=> '\f27e',
+	),
+	'buysellads.com'	=> array(
+		'color'		=> '#c90100',
+		'name'		=> 'buysellads',
+		'unicode'	=> '\f20d',
+	),
+	'codepen.io'	=> array(
+		'color'		=> '#000000',
+		'name'		=> 'codepen',
+		'unicode'	=> '\f1cb',
+	),
+	'codiepie.com'	=> array(
+		'color'		=> '#000000',
+		'name'		=> 'codiepie',
+		'unicode'	=> '\f284',
+	),
+	'connectdevelop.com'	=> array(
+		'color'		=> '#391448',
+		'name'		=> 'connectdevelop',
+		'unicode'	=> '\f20e',
+	),
+	'contao.org'	=> array(
+		'color'		=> '#f47c00',
+		'name'		=> 'contao',
+		'unicode'	=> '\f26d',
+	),
+	'dashcube.com'	=> array(
+		'color'		=> '#7f7f7f',
+		'name'		=> 'dashcube',
+		'unicode'	=> '\f210',
+	),
+	'del.icio.us'	=> array(
+		'color'		=> '#0076e8',
+		'name'		=> 'delicious',
+		'unicode'	=> '\f1a5',
+	),
+	'deviantart.com'	=> array(
+		'color'		=> '#05cc47',
+		'name'		=> 'deviantart',
+		'unicode'	=> '\f1bd',
+	),
+	'digg.com'	=> array(
+		'color'		=> '#005be2',
+		'name'		=> 'digg',
+		'unicode'	=> '\f1a6',
+	),
+	'dribbble.com'	=> array(
+		'color'		=> '#ea4c89',
+		'name'		=> 'dribbble',
+		'unicode'	=> '\f17d',
+	),
+	'dropbox.com'	=> array(
+		'color'		=> '#1081de',
+		'name'		=> 'dropbox',
+		'unicode'	=> '\f16b',
+	),
+	'drupal.org'	=> array(
+		'color'		=> '#0678be',
+		'name'		=> 'drupal',
+		'unicode'	=> '\f1a9',
+	),
+	'facebook.com'	=> array(
+		'color'		=> '#3b5998',
+		'name'		=> 'facebook',
+		'unicode'	=> '\f09a',
+	),
+	'flickr.com'	=> array(
+		'color'		=> '#128fdc',
+		'name'		=> 'flickr',
+		'unicode'	=> '\f16e',
+	),
+	'foursquare.com'	=> array(
+		'color'		=> '#2d5be3',
+		'name'		=> 'foursquare',
+		'unicode'	=> '\f180',
+	),
+	'getpocket.com'	=> array(
+		'color'		=> '#ee4056',
+		'name'		=> 'get-pocket',
+		'unicode'	=> '\f265',
+	),
+	'git-scm.com'	=> array(
+		'color'		=> '#f54d27',
+		'name'		=> 'git',
+		'unicode'	=> '\f1d3',
+	),
+	'github.com'	=> array(
+		'color'		=> '#333333',
+		'name'		=> 'github',
+		'unicode'	=> '\f09b',
+	),
+	'gitlab.com'	=> array(
+		'color'		=> '#e24329',
+		'name'		=> 'gitlab',
+		'unicode'	=> '\f296',
+	),
+	'glide.me'	=> array(
+		'color'		=> '#00baff',
+		'name'		=> 'glide-g',
+		'unicode'	=> '\f2a6',
+	),
+	'plus.google.com'	=> array(
+		'color'		=> '#db4437',
+		'name'		=> 'google-plus',
+		'unicode'	=> '\f0d5',
+	),
+	'gratipay.com'	=> array(
+		'color'		=> '#663300',
+		'name'		=> 'gratipay',
+		'unicode'	=> '\f184',
+	),
+	'news.ycombinator.com'	=> array(
+		'color'		=> '#ff6600',
+		'name'		=> 'hacker-news',
+		'unicode'	=> '\f1d4',
+	),
+	'houzz.com'	=> array(
+		'color'		=> '#55a32a',
+		'name'		=> 'houzz',
+		'unicode'	=> '\f27c',
+	),
+	'instagram.com'	=> array(
+		'color'		=> '#e40b23',
+		'name'		=> 'instagram',
+		'unicode'	=> '\f16d',
+	),
+	'iox.host'	=> array(
+		'color'		=> '#faa829',
+		'name'		=> 'ioxhost',
+		'unicode'	=> '\f208',
+	),
+	'joomla.org'	=> array(
+		'color'		=> '#1a3b6b',
+		'name'		=> 'joomla',
+		'unicode'	=> '\f1aa',
+	),
+	'jsfiddle.net'	=> array(
+		'color'		=> '#1C90F3',
+		'name'		=> 'jsfiddle',
+		'unicode'	=> '\f1cc',
+	),
+	'last.fm'	=> array(
+		'color'		=> '#B8FFFF',
+		'name'		=> 'lastfm',
+		'unicode'	=> '\f202',
+	),
+	'leanpub.com'	=> array(
+		'color'		=> '#231f20',
+		'name'		=> 'leanpub',
+		'unicode'	=> '\f212',
+	),
+	'linkedin.com'	=> array(
+		'color'		=> '#0177b5',
+		'name'		=> 'linkedin',
+		'unicode'	=> '\f0e1',
+	),
+	'linuxfoundation.org'	=> array(
+		'color'		=> '#003764',
+		'name'		=> 'linux',
+		'unicode'	=> '\f17c',
+	),
+	'maxcdn.com'	=> array(
+		'color'		=> '#ff5b02',
+		'name'		=> 'maxcdn',
+		'unicode'	=> '\f136',
+	),
+	'medium.com'	=> array(
+		'color'		=> '#02b875',
+		'name'		=> 'medium',
+		'unicode'	=> '\f23a',
+	),
+	'mixcloud.com'	=> array(
+		'color'		=> '#34495e',
+		'name'		=> 'mixcloud',
+		'unicode'	=> '\f289',
+	),
+	'modx.com'	=> array(
+		'color'		=> '#79b14c',
+		'name'		=> 'modx',
+		'unicode'	=> '\f285',
+	),
+	'ok.ru'	=> array(
+		'color'		=> '#f58220',
+		'name'		=> 'odnoklassniki',
+		'unicode'	=> '\f263',
+	),
+	'odnoklassniki.ru'	=> array(
+		'color'		=> '#f58220',
+		'name'		=> 'odnoklassniki',
+		'unicode'	=> '\f263',
+	),
+	'opencart.com'	=> array(
+		'color'		=> '#28c2f0',
+		'name'		=> 'opencart',
+		'unicode'	=> '\f23d',
+	),
+	'openid.net'	=> array(
+		'color'		=> '#f28018',
+		'name'		=> 'openid',
+		'unicode'	=> '\f19b',
+	),
+	'optinmonster.com'	=> array(
+		'color'		=> '#0d82df',
+		'name'		=> 'optin-monster',
+		'unicode'	=> '\f23c',
+	),
+	'paypal.com'	=> array(
+		'color'		=> '#071947',
+		'name'		=> 'paypal',
+		'unicode'	=> '\f1ed',
+	),
+	'pinterest.com'	=> array(
+		'color'		=> '#bb0f23',
+		'name'		=> 'pinterest',
+		'unicode'	=> '\f0d2',
+	),
+	'producthunt.com'	=> array(
+		'color'		=> '#da552f',
+		'name'		=> 'product-hunt',
+		'unicode'	=> '\f288',
+	),
+	'qq.com'	=> array(
+		'color'		=> '#5da4e6',
+		'name'		=> 'qq',
+		'unicode'	=> '\f1d6',
+	),
+	'reddit.com'	=> array(
+		'color'		=> '#ff4500',
+		'name'		=> 'reddit-alien',
+		'unicode'	=> '\f281',
+	),
+	'renren.com'	=> array(
+		'color'		=> '#217dc6',
+		'name'		=> 'renren',
+		'unicode'	=> '\f18b',
+	),
+	'scribd.com'	=> array(
+		'color'		=> '#1a7bba',
+		'name'		=> 'scribd',
+		'unicode'	=> '\f28a',
+	),
+	'sellsy.com'	=> array(
+		'color'		=> '#26c6d0',
+		'name'		=> 'sellsy',
+		'unicode'	=> '\f213',
+	),
+	'shirtsinbulk.com'	=> array(
+		'color'		=> '#db3c2e',
+		'name'		=> 'shirtsinbulk',
+		'unicode'	=> '\f214',
+	),
+	'simplybuilt.com'	=> array(
+		'color'		=> '#000000',
+		'name'		=> 'simplybuilt',
+		'unicode'	=> '\f215',
+	),
+	'skyatlas.com'	=> array(
+		'color'		=> '#08455b',
+		'name'		=> 'skyatlas',
+		'unicode'	=> '\f216',
+	),
+	'skype.com'	=> array(
+		'color'		=> '#00aff0',
+		'name'		=> 'skype',
+		'unicode'	=> '\f17e',
+	),
+	'slack.com'	=> array(
+		'color'		=> '#e51870',
+		'name'		=> 'slack',
+		'unicode'	=> '\f198',
+	),
+	'slideshare.net'	=> array(
+		'color'		=> '#10384e',
+		'name'		=> 'slideshare',
+		'unicode'	=> '\f1e7',
+	),
+	'snapchat.com'	=> array(
+		'color'		=> '#fff937',
+		'name'		=> 'snapchat-ghost',
+		'unicode'	=> '\f2ac',
+	),
+	'soundcloud.com'	=> array(
+		'color'		=> '#fb641f',
+		'name'		=> 'soundcloud',
+		'unicode'	=> '\f1be',
+	),
+	'spotify.com'	=> array(
+		'color'		=> '#7c25f8',
+		'name'		=> 'spotify',
+		'unicode'	=> '\f1bc',
+	),
+	'stackexchange.com'	=> array(
+		'color'		=> '#4679b7',
+		'name'		=> 'stack-exchange',
+		'unicode'	=> '\f18d',
+	),
+	'stackoverflow.com'	=> array(
+		'color'		=> '#f48024',
+		'name'		=> 'stack-overflow',
+		'unicode'	=> '\f16c',
+	),
+	'steampowered.com'	=> array(
+		'color'		=> '#171a21',
+		'name'		=> 'steam',
+		'unicode'	=> '\f1b6',
+	),
+	'stumbleupon.com'	=> array(
+		'color'		=> '#eb4924',
+		'name'		=> 'stumbleupon',
+		'unicode'	=> '\f1a4',
+	),
+	't.qq.com'	=> array(
+		'color'		=> '#3d95d5',
+		'name'		=> 'tencent-weibo',
+		'unicode'	=> '\f1d5',
+	),
+	'themeisle.com'	=> array(
+		'color'		=> '#189ac4',
+		'name'		=> 'themeisle',
+		'unicode'	=> '\f2b2',
+	),
+	'trello.com'	=> array(
+		'color'		=> '#0079bf',
+		'name'		=> 'trello',
+		'unicode'	=> '\f181',
+	),
+	'tripadvisor.com'	=> array(
+		'color'		=> '#589442',
+		'name'		=> 'tripadvisor',
+		'unicode'	=> '\f262',
+	),
+	'tumblr.com'	=> array(
+		'color'		=> '#56BC8B',
+		'name'		=> 'tumblr',
+		'unicode'	=> '\f173',
+	),
+	'twitch.tv'	=> array(
+		'color'		=> '#6441a5',
+		'name'		=> 'twitch',
+		'unicode'	=> '\f1e8',
+	),
+	'twitter.com'	=> array(
+		'color'		=> '#1da1f2',
+		'name'		=> 'twitter',
+		'unicode'	=> '\f099',
+	),
+	'viacoin.org'	=> array(
+		'color'		=> '#2176a5',
+		'name'		=> 'viacoin',
+		'unicode'	=> '\f237',
+	),
+	'viadeo.com'	=> array(
+		'color'		=> '#f07355',
+		'name'		=> 'viadeo',
+		'unicode'	=> '\f2a9',
+	),
+	'vimeo.com'	=> array(
+		'color'		=> '#00b3ec',
+		'name'		=> 'vimeo',
+		'unicode'	=> '\f27d',
+	),
+	'vine.co'	=> array(
+		'color'		=> '#00bf8f',
+		'name'		=> 'vine',
+		'unicode'	=> '\f1ca',
+	),
+	'vk.com'	=> array(
+		'color'		=> '#4c75a3',
+		'name'		=> 'vk',
+		'unicode'	=> '\f189',
+	),
+	'wechat.com'	=> array(
+		'color'		=> '#4ec034',
+		'name'		=> 'wechat',
+		'unicode'	=> '\f1d7',
+	),
+	'weibo.com'	=> array(
+		'color'		=> '#e6162d',
+		'name'		=> 'weibo',
+		'unicode'	=> '\f18a',
+	),
+	'wx.qq.com'	=> array(
+		'color'		=> '#4ec034',
+		'name'		=> 'weixin',
+		'unicode'	=> '\f1d7',
+	),
+	'whatsapp.com'	=> array(
+		'color'		=> '#00e676',
+		'name'		=> 'whatsapp',
+		'unicode'	=> '\f232',
+	),
+	'wikipedia.org'	=> array(
+		'color'		=> '#000000',
+		'name'		=> 'wikipedia-w',
+		'unicode'	=> '\f266',
+	),
+	'wordpress.com'	=> array(
+		'color'		=> '#0087be',
+		'name'		=> 'wordpress',
+		'unicode'	=> '\f19a',
+	),
+	'wordpress.org'	=> array(
+		'color'		=> '#21759b',
+		'name'		=> 'wordpress',
+		'unicode'	=> '\f19a',
+	),
+	'wpbeginner.com'	=> array(
+		'color'		=> '#2d2520',
+		'name'		=> 'wpbeginner',
+		'unicode'	=> '\f297',
+	),
+	'xing.com'	=> array(
+		'color'		=> '#005453',
+		'name'		=> 'xing',
+		'unicode'	=> '\f168',
+	),
+	'ycombinator.com'	=> array(
+		'color'		=> '#f26522',
+		'name'		=> 'y-combinator',
+		'unicode'	=> '\f23b',
+	),
+	'yahoo.com'	=> array(
+		'color'		=> '#4101af',
+		'name'		=> 'yahoo',
+		'unicode'	=> '\f19e',
+	),
+	'yelp.com'	=> array(
+		'color'		=> '#c41200',
+		'name'		=> 'yelp',
+		'unicode'	=> '\f1e9',
+	),
+	'yoast.com'	=> array(
+		'color'		=> '#77b227',
+		'name'		=> 'yoast',
+		'unicode'	=> '\f2b1',
+	),
+	'youtube.com'	=> array(
+		'color'		=> '#d62422',
+		'name'		=> 'youtube-play',
+		'unicode'	=> '\f16a',
+	),
+);
